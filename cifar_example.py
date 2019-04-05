fb20a1e682e8ae163ead362062f36def8d805c87
Kyle Pearson <pearsonk@email.arizona.edu>
2019-04-04 21:58:20 -0700

    added cifar example

---
+++ cifar_example.py
@@ -0,0 +1,21 @@
+from keras.datasets import cifar10
+from dcgan import DCGAN
+
+if __name__ == '__main__':
+
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    # only birds, then scale images between 0-1
+    x_train = x_train[ (y_train==2).reshape(-1) ] 
+    x_train = x_train/255
+    
+    # scale RGB data between 0 and 1
+    x_train /= 255 
+    
+    dcgan = DCGAN(img_rows = x_train[0].shape[0],
+                    img_cols = x_train[0].shape[1],
+                    channels = x_train[0].shape[2], 
+                    latent_dim=128,
+                    name='cifar10')
+
+    dcgan.train(x_train, epochs=10001, batch_size=32, save_interval=500)
--- dcgan.py
+++ dcgan.py
@@ -174,7 +174,8 @@ class DCGAN():
             g_loss = self.combined.train_on_batch(noise, valid)
 
             # Plot the progress
-            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+            if epoch % 10 == 0:
+                print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
 
             # If at save interval => save generated image samples
             if epoch % save_interval == 0:
