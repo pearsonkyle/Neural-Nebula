2a9f3708b874b8eb3c5ccf5a667a4168ced48903
Kyle Pearson <pearsonk@email.arizona.edu>
2019-04-04 21:37:13 -0700

    source code

---
+++ dcgan_train.py
@@ -0,0 +1,33 @@
+import argparse
+
+import tensorflow as tf
+
+from dcgan import DCGAN, export_model, create_dataset
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    help_ = "Number of training epochs"
+    parser.add_argument("-e", "--epochs", help=help_, default=1000, type=int)
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+
+    # parse arguments
+    args = parse_args()
+    if args is None:
+      exit()
+
+    x_train, y_train = create_dataset(128,128, nSlices=150, resize=0.75, directory='space/') # 3 channels = RGB
+    assert(x_train.shape[0]>0)
+
+    x_train /= 255 
+
+    dcgan = DCGAN(img_rows = x_train[0].shape[0],
+                    img_cols = x_train[0].shape[1],
+                    channels = x_train[0].shape[2], 
+                    latent_dim=32,
+                    name='nebula_32_128')
+                    
+    dcgan.train(x_train, epochs=args.epochs, batch_size=32, save_interval=100)
